4b212424931cf61acc0ec51a676e95ee42082a07
Alessio Palomba <80175491+NoFortunateSon@users.noreply.github.com>
2022-09-30 12:45:08 +0200

    added routing, home/layout/shared modules

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ArtistCardComponent } from './artist-card/artist-card.component';
+
+
+
+@NgModule({
+  declarations: [
+    ArtistCardComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
