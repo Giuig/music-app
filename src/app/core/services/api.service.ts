e4327627215bdbdae2073cdf318ea3beed7c03eb
Giulio <giulio.gambino@unikey.it>
2022-10-03 11:11:15 +0200

    added search services (artist, album, tracks)

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -6,12 +6,24 @@ import {HttpClient} from "@angular/common/http";
 })
 export class ApiService {
 
-  baseUrl = "https://ws.audioscrobbler.com"
+  baseUrl = "https://ws.audioscrobbler.com/2.0/"
   API_KEY_LASTFM = "dfb6f837358c6046fe38ef728941bf25";
 
   constructor(private httpClient: HttpClient) {}
 
   getTopTracks(){
-    return this.httpClient.get(this.baseUrl + "/2.0/?method=chart.gettoptracks&api_key=" + this.API_KEY_LASTFM +"&format=json")
+    return this.httpClient.get(this.baseUrl + '?method=chart.gettoptracks&api_key=' + this.API_KEY_LASTFM + '&format=json');
+  }
+
+  getArtistSearch(search: string){
+    return this.httpClient.get(this.baseUrl + '?method=artist.search&artist=' + search + '&api_key=' + this.API_KEY_LASTFM + '&format=json');
+  }
+
+  getAlbumSearch(search:string){
+    return this.httpClient.get(this.baseUrl + '?method=album.search&album=' + search + '&api_key=' + this.API_KEY_LASTFM + '&format=json')
+  }
+
+  getTrackSearch(search:string){
+    return this.httpClient.get(this.baseUrl + '?method=track.search&track=' + search + '&api_key=' + this.API_KEY_LASTFM + '&format=json')
   }
 }
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -9,20 +9,72 @@ import {Subscription} from "rxjs";
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService) {
+  }
 
   topTracksSubscription: Subscription | undefined;
   topTracks: any;
+  searchParam: any;
+  artistSearchResultsSubscription: Subscription | undefined;
+  artistSearchResults: any;
+  albumSearchResultsSubscription: Subscription | undefined;
+  albumSearchResults: any;
+  trackSearchResultsSubscription: Subscription | undefined;
+  trackSearchResults: any;
 
   ngOnInit(): void {
   }
 
   getTopTracks() {
     this.topTracksSubscription = this.apiService.getTopTracks().subscribe(observer => {
-      this.topTracks = {...observer};
-      console.log(this.topTracks)
+        this.topTracks = {...observer};
+        console.log(this.topTracks)
       },
-      () => {},
+      () => {
+      },
     )
+  }
+
+  getArtistSearch(search: string) {
+    this.artistSearchResultsSubscription = this.apiService.getArtistSearch(search).subscribe(observer => {
+        this.artistSearchResults = {...observer};
+        console.log(this.artistSearchResults)
+      },
+      () => {
+      },
+    )
+  }
+
+  getAlbumSearch(search: string) {
+    this.albumSearchResultsSubscription = this.apiService.getAlbumSearch(search).subscribe(observer => {
+        this.albumSearchResults = {...observer};
+        console.log(this.albumSearchResults)
+      },
+      () => {
+      },
+    )
+  }
+
+  getTrackSearch(search: string) {
+    this.trackSearchResultsSubscription = this.apiService.getTrackSearch(search).subscribe(observer => {
+        this.trackSearchResults = {...observer};
+        console.log(this.trackSearchResults)
+      },
+      () => {
+      },
+    )
+  }
+
+  search($event: KeyboardEvent) {
+    // @ts-ignore
+    console.log($event.target.value)
+    // @ts-ignore
+    this.searchParam = $event.target.value;
+  }
+
+  getSearch(search: string){
+    this.getArtistSearch(search);
+    this.getAlbumSearch(search);
+    this.getTrackSearch(search);
   }
 }
